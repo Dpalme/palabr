4c806ca4d812b7ea21c3b55fc3ad6f0521b1a924
Dpalme <diegoPalmerin24@gmail.com>
2023-05-25 11:16:54 -0600

    reworked random word algorithm

--- src/hooks/useDailyWord.ts
+++ src/hooks/useDailyWord.ts
@@ -1,5 +1,12 @@
 import wordList from '../assets/filtered_05.json';
 import md5 from 'md5';
+import { useToday } from './useToday';
+
+function getWordForISODate(isoDate: string) {
+  var hashStr = md5(isoDate),
+    hash = parseInt(hashStr, 16);
+  return wordList[hash % wordList.length];
+}
 
 export const useDailyWord = () => {
   const today = useToday();
